59fc316a729f796236fb1c300afb6f6b5ab44737
pnts <panagiotis.tsivelekidis@gmail.com>
2022-01-05 09:58:34 +0100

    create and pass state

--- bot.js
+++ bot.js
@@ -3,25 +3,15 @@ const BOT_API_KEY = '5058747304:AAHBseew7Rf_1jljK2Uui6Ept5W9mPzt6u8'
 
 const bot = new Telegraf(BOT_API_KEY)
 
-bot.start((ctx) => {
+bot.start((ctx, next) => {
+  ctx.state.myState = 1234
   ctx.reply('You have entered the start command')
-})
-
-bot.command(['myCommand', 'test', 'Test'], (ctx, next) => {
-  ctx.reply('This is my command')
   next(ctx)
-})
-
-bot.hears('cat', (ctx) => {
-  ctx.reply('meow')
 })
 
 bot.on('text', (ctx) => {
   ctx.reply('This is a text message')
-})
-
-bot.on('sticker', (ctx) => {
-  ctx.reply('Nice sticker bro')
+  ctx.reply(ctx.state.myState)
 })
 
 bot.launch()
