b5ed691dcd04019a08cb11351803d89451198b6a
pnts <panagiotis.tsivelekidis@gmail.com>
2022-01-05 09:11:36 +0100

    simple reply on start command

--- bot.js
+++ bot.js
@@ -3,10 +3,7 @@ const BOT_API_KEY = '5058747304:AAHBseew7Rf_1jljK2Uui6Ept5W9mPzt6u8'
 
 const bot = new Telegraf(BOT_API_KEY)
 
-// console.log(bot);
-
 bot.start((ctx) => {
-//   console.log(ctx);
   ctx.reply('You have entered the start command')
 })
 
